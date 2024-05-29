8a21c58556d8c4796d0fc3d25d760ab87ee840bc
dpowers1 <dmnckpwrs@gmail.com>
2024-05-29 14:08:30 -0400

    Add some basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,10 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import ProductGrid from "./components/ProductGrid";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Header />
       <RouterProvider router={router} />
-      <ProductGrid />
-      <Footer />
     </>
   );
 }
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+
+import Root from "./Root";
+import ProductGrid from "./components/ProductGrid";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/products",
+        element: <ProductGrid />,
+      },
+    ],
   },
 ]);
 
