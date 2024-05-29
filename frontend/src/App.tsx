783fd971c1a8fd9c0988e80685d7d3f2270167d8
dpowers1 <dmnckpwrs@gmail.com>
2024-05-29 13:46:25 -0400

    Add scaffold for router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ProductGrid from "./components/ProductGrid";
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
+
 function App() {
   return (
-    <div>
+    <>
       <Header />
+      <RouterProvider router={router} />
       <ProductGrid />
       <Footer />
-    </div>
+    </>
   );
 }
 
